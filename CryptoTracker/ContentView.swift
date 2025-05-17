683b0288f458ddb4c4843527f10c0e73e92cbc61
hemanth895 <hemanthreddy1331@gmail.com>
2025-05-17 18:18:25 +0530

    added few lines

--- CryptoTracker/ContentView.swift
+++ CryptoTracker/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        
         ZStack{
             Color.theme.background
                 .ignoresSafeArea()
--- CryptoTracker/Utilities/LocalFileManager.swift
+++ CryptoTracker/Utilities/LocalFileManager.swift
@@ -18,6 +18,7 @@ class LocalFileManager {
     func saveImage(image:UIImage,imageName:String,folderName:String){
         
         
+        
         createFolderIfNeeded(folderName: folderName)
         
         
